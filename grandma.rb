28cd63d6dd97916c1c0a5151e76db0cf097eeb41
Dennis Lord <lorddennis1006@gmail.com>
2018-04-30 23:09:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(speak)
     "NO, NOT SINCE 1938"
   elsif speak == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  else
+  elseif speak == speak.lowercase
     "HUH?! SPEAK UP, SONNY!"
    end
  end
