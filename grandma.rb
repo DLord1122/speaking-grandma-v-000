0b8af402f567698f0e6a4a0d5c09dd3c0387febd
Dennis Lord <lorddennis1006@gmail.com>
2018-04-30 23:13:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speak)
-  if speak == speak.upcase
+  if speak != speak.upcase
     "NO, NOT SINCE 1938!"
   elsif speak == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
